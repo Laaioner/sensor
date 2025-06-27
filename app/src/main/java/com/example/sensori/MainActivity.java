f8fd554e9efc947ba918c97d1e46333624b8e3f7
laaioner <pedropiovesan2002@gmail.com>
2025-06-26 22:08:32 -0300

    sensor - localização

--- app/src/main/java/com/example/sensori/MainActivity.java
+++ app/src/main/java/com/example/sensori/MainActivity.java
@@ -1,57 +1,63 @@
 package com.example.sensori;
 
+import android.Manifest;
+import android.app.Activity;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
-import android.hardware.SensorManager;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.widget.*;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.core.app.ActivityCompat;
 
-public class MainActivity extends AppCompatActivity implements SensorEventListener {
-//Adicionar giroscópio e acelerômetro
-    SensorManager sensorManager;
-    TextView tv, tvAce, tvGir;
+public class MainActivity extends AppCompatActivity {
+    LocationManager lm;
+    TextView tvLatitude, tvLongitude;
+
+    Button bnt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        tv=findViewById(R.id.tvResultado);
-        tvAce=findViewById(R.id.tvAce);
-        tvGir=findViewById(R.id.tvGir);
-        sensorManager=(SensorManager) getSystemService(Context.SENSOR_SERVICE);
-        Sensor sensorLuz = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
-        Sensor sensorAce = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-        Sensor sensorGir = sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
-        sensorManager.registerListener(this,sensorLuz,SensorManager.SENSOR_DELAY_NORMAL);
-        sensorManager.registerListener(this,sensorAce,SensorManager.SENSOR_DELAY_NORMAL);
-        sensorManager.registerListener(this,sensorGir,SensorManager.SENSOR_DELAY_NORMAL);
+        tvLongitude = findViewById(R.id.tvLongitude);
+        tvLatitude = findViewById(R.id.tvLatitude);
+        bnt = findViewById(R.id.button);
+
+        lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+        bnt.setOnClickListener(v -> getLocalizacao());
+
     }
 
-    @Override
-    public void onSensorChanged(SensorEvent sensorEvent) {
-        if(sensorEvent.sensor.getType()==Sensor.TYPE_LIGHT){
-            tv.setText(Float.toString((sensorEvent.values[0])));
+    public void getLocalizacao() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED ||
+                ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED)
+        {
+            ActivityCompat.requestPermissions(
+                    this,
+                    new String[]{
+                            Manifest.permission.ACCESS_FINE_LOCATION,
+                            Manifest.permission.ACCESS_COARSE_LOCATION
+                    },
+                    1
+            );
         }
+        Location location = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
 
-        if (sensorEvent.sensor.getType()==Sensor.TYPE_ACCELEROMETER){
-            tvAce.setText(Float.toString(sensorEvent.values[1]));
+        if(location!=null){
+            tvLatitude.setText(Double.toString(location.getLatitude()));
+            tvLongitude.setText(Double.toString(location.getLatitude()));
+        }else {
+            tvLatitude.setText("erro");
         }
-
-        if (sensorEvent.sensor.getType()==Sensor.TYPE_GYROSCOPE) {
-            tvGir.setText(Float.toString(sensorEvent.values[2]));
-        }
-    }
-    @Override
-    public void onAccuracyChanged(Sensor sensor, int i) {
-
     }
 }
+
